969b8026a09beddc3121c81506e019b81a4c5ad7
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-06-02 23:42:55 -0300

    basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello world" }));
     println!("Hello, world!");
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap()
 }
