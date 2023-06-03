5bbb836bdf2be5688ad665809f16e14d35bbd906
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-06-02 23:53:50 -0300

    Add a few endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{extract::Path, routing::get, Router};
+
+async fn greet(Path(username): Path<String>) -> String {
+    format!("Hello {}", username)
+}
+
+async fn health_check() -> &'static str {
+    "Hello world"
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello world" }));
+    let app = Router::new()
+        .route("/health_check", get(health_check))
+        .route("/:name", get(greet));
     println!("Hello, world!");
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
